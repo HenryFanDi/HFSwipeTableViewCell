7afee9a3ed63872f05b87012b7301b53caff2881
Henry Fan <henry.fan@centraltrillion.com>
2016-08-18 06:34:12 +0800

    Initialize Project

---
+++ HFSwipeTableViewCell/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HFSwipeTableViewCell
+//
+//  Created by HenryFan on 18/8/2016.
+//  Copyright © 2016 HenryFanDi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
