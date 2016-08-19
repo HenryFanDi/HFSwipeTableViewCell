75508691c87bf00b7550e07b9a574ef4df133a78
Henry Fan <henry.fan@centraltrillion.com>
2016-08-20 06:19:57 +0800

    feat: add HFSwipeTableViewCell

---
+++ HFSwipeTableViewCell/HFSwipeTableViewCell.swift
@@ -0,0 +1,29 @@
+//
+//  HFSwipeTableViewCell.swift
+//  HFSwipeTableViewCell
+//
+//  Created by HenryFan on 20/8/2016.
+//  Copyright © 2016 HenryFanDi. All rights reserved.
+//
+
+import UIKit
+
+class HFSwipeTableViewCell: UITableViewCell {
+  
+  // MARK: Lifecycle
+  
+  override func awakeFromNib() {
+    super.awakeFromNib()
+    setupSwipeTableViewCell()
+  }
+  
+  override func setSelected(selected: Bool, animated: Bool) {
+    super.setSelected(selected, animated: animated)
+  }
+  
+  // MARK: Private
+  
+  private func setupSwipeTableViewCell() {
+  }
+  
+}
