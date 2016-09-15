9d3e296fced3793199af6512b5318528739c6940
Henry Fan <henry.fan@centraltrillion.com>
2016-09-15 09:55:15 +0800

    feat: add HFSwipeButton

---
+++ HFSwipeTableViewCell/HFSwipeButton.swift
@@ -0,0 +1,95 @@
+//
+//  HFSwipeButton.swift
+//  HFSwipeTableViewCell
+//
+//  Created by HenryFan on 15/9/2016.
+//  Copyright © 2016 HenryFanDi. All rights reserved.
+//
+
+import UIKit
+
+enum HFSwipeButtonType {
+  case HFSwipeButtonUnknown
+  case HFSwipeButtonDial
+}
+
+class HFSwipeButton: UIButton {
+  
+  private var swipeButtonType: HFSwipeButtonType
+  private let swipeImageView: UIImageView
+  private var swipeImageSize: CGFloat
+  
+  // MARK: Lifecycle
+  
+  convenience required init?(coder aDecoder: NSCoder) {
+    self.init(swipeButtonType: .HFSwipeButtonUnknown)
+  }
+  
+  init(swipeButtonType: HFSwipeButtonType) {
+    self.swipeButtonType = swipeButtonType
+    self.swipeImageView = UIImageView()
+    self.swipeImageSize = 0.0
+    super.init(frame: CGRectZero)
+  }
+  
+  // MARK: Private
+  
+  private func setupButton() {
+    setupSwipeButton()
+    setupSwipeImageView()
+    setupSwipeButtonWithSwipeButtonType()
+    setupSwipeButtonConstraints()
+  }
+  
+  private func setupSwipeButton() {
+    translatesAutoresizingMaskIntoConstraints = false
+  }
+  
+  private func setupSwipeImageView() {
+    swipeImageSize = 20.0
+    swipeImageView.translatesAutoresizingMaskIntoConstraints = false
+    addSubview(swipeImageView)
+  }
+  
+  private func setupSwipeButtonWithSwipeButtonType() {
+    var imageNamed = ""
+    switch swipeButtonType {
+    case .HFSwipeButtonDial:
+      imageNamed = "Dial"
+      break
+    default:
+      break
+    }
+    self.swipeImageView.image = UIImage.init(named: imageNamed)
+  }
+  
+  private func setupSwipeButtonConstraints() {
+    swipeImageView.addConstraint(
+      NSLayoutConstraint.init(item: swipeImageView,
+        attribute: .Height,
+        relatedBy: .Equal,
+        toItem: nil,
+        attribute: .Height,
+        multiplier: 1.0,
+        constant: swipeImageSize)
+    )
+    
+    addConstraints([
+      NSLayoutConstraint.init(item: swipeImageView,
+        attribute: .CenterX,
+        relatedBy: .Equal,
+        toItem: self,
+        attribute: .CenterX,
+        multiplier: 1.0,
+        constant: 0.0),
+      NSLayoutConstraint.init(item: swipeImageView,
+        attribute: .CenterY,
+        relatedBy: .Equal,
+        toItem: self,
+        attribute: .CenterY,
+        multiplier: 1.0,
+        constant: 0.0)
+      ])
+  }
+  
+}
